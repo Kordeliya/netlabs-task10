5647efe0cfac05d41826d801798269cbca860ad9
Koreliya <miss-neismit1991@yandex.ru>
2016-08-07 22:13:07 +0500

    Add Solution

---
+++ Zueva_Natalia_ORM/ORM/Attributes/ColumnAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class ColumnAttribute : Attribute
+    {
+        private string columnName;
+
+        public ColumnAttribute(string name)
+        {
+            columnName = name;
+        }
+    }
+}
---
+++ Zueva_Natalia_ORM/ORM/DBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    public abstract class DBContext
+    {
+        public DBContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public string ConnectionString { get; private set; }
+
+        public Lazy<
+    }
+}
